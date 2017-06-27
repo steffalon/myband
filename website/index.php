3e9aa9e9c9552eae04cc45365668cbcb39e9b395
steffalon <simon.boerrigter@gmail.com>
2017-06-27 11:32:47 +0200

    meta tags update

--- website/index.php
+++ website/index.php
@@ -17,8 +17,7 @@ if (!empty($_GET['page'])) {
     <link rel="stylesheet" href="css/main.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="Dit is een nieuws pagina over auto's. Hier vind je recente auto nieuws die op de
-    markt zijn gekomen.">
+    <meta name="description" content="Dit is een nieuws pagina over auto's. Hier vind je recente auto nieuws die op de markt zijn gekomen.">
     <meta name="keywords" content="Auto's, Nieuws, CarTek">
 </head>
 <body>
